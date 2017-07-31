75537ac4c7c584ac8ec7d0763135541933bb5868
Sean Larkin <sean.larkin@cuw.edu>
2017-07-30 23:53:23 -0500

    feat(loader): returned loader to original state from master branch

--- loader.js
+++ loader.js
@@ -1,8 +1,7 @@
 const loaderUtils = require("loader-utils");
 
 module.exports = function loader(source) {
-    const webpackRemainingChain = loaderUtils.getRemainingRequest(this);
+    console.log(source);
 
-    console.log(`REMAINING CHAIN: ${webpackRemainingChain}`);
     return source;
 }
