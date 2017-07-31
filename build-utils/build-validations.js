0b65879d346c079042a2efe68c6274210c5e6584
Sean Larkin <sean.larkin@cuw.edu>
2017-07-31 07:17:02 -0500

    feat(errors): update validation error to hint at running yarn scripts

--- build-utils/build-validations.js
+++ build-utils/build-validations.js
@@ -1,5 +1,7 @@
 const chalk = require("chalk");
-const ERR_NO_ENV_FLAG = chalk.red(`You must pass an --env.env flag into your build for webpack to work!`);
+const ERR_NO_ENV_FLAG = chalk.red(
+    `You must pass an --env.env flag into your build for webpack to work! Try running 'yarn build' or 'yarn dev' for starters!`
+);
 
 module.exports = {
     ERR_NO_ENV_FLAG
