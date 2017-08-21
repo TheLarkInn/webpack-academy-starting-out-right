8a5f279aadc7c7e708f5ecb8425ed65326914bcf
Sean Larkin <sean.larkin@cuw.edu>
2017-08-21 16:17:03 -0500

    feat(deps&tag): update deps, finishing informative loader

--- build-utils/addons/webpack.customloader.js
+++ build-utils/addons/webpack.customloader.js
@@ -1,22 +1,27 @@
 // @ts-nocheck
 
 const config = {
-    resolveLoader: {
-        alias: {
-            "our-first-loader": require.resolve("../../loader.js")
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.md/,
-                enforce: "pre",
-                use: [
-                    {loader: "our-first-loader"}
-                ]
-            }
-        ]
+  resolveLoader: {
+    alias: {
+      "our-first-loader": require.resolve("../../loader.js")
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.md/,
+        enforce: "pre",
+        use: [
+          {
+            loader: "our-first-loader",
+            options: {
+              warnings: true
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
 
 module.exports = config;
--- loader.js
+++ loader.js
@@ -1,13 +1,18 @@
+const loaderUtils = require("loader-utils");
 const checkSpellingAsync = require("./checkSpellingAsync");
 
 module.exports = function loader(source) {
-    const callback = this.async();
-    const {emitWarning} = this; // this.emitWarning()
+  const callback = this.async();
+  const { emitWarning } = this; // this.emitWarning()
+  const options = loaderUtils.getOptions(this); //'this' is the Loader Context itself.
 
-    checkSpellingAsync(source, (misspelledWords) => {
-        const words = Array.from(misspelledWords.keys()).join(" | ");
+  checkSpellingAsync(source, (misspelledWords) => {
+    const words = Array.from(misspelledWords.keys()).join(" | ");
 
-        emitWarning(words); 
-        callback(null, source);
-    })
+    if (options.warnings) {
+      emitWarning(words);
+    }
+
+    callback(null, source);
+  })
 }
